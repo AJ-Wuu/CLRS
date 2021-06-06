c5f8d13636d0e9a71af8573cde713bd1dd5a7cb7
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-07 01:13:29 +0800

    Update Supplement.java

--- Chapter13-Red-Black Trees/Simple Implementation/Supplement.java
+++ Chapter13-Red-Black Trees/Simple Implementation/Supplement.java
@@ -13,11 +13,11 @@ public class Supplement {
 		}
 	}
 	
-	public static <T> void midOrder(Node<T> root) {
+	public static <T> void inOrder(Node<T> root) {
 		if (root != null) {
-			midOrder(root.left);
+			inOrder(root.left);
 			System.out.print(root.key + " ");
-			midOrder(root.right);
+			inOrder(root.right);
 		}
 	}
 	

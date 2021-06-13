c1de9f7258a2cde21e2cdb47fb1c6d5edad1550a
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-13 18:37:11 +0800

    Update ActivitySelection.java

--- Chapter16-Greedy Algorithms/ActivitySelection.java
+++ Chapter16-Greedy Algorithms/ActivitySelection.java
@@ -6,6 +6,9 @@ package activitySelection;
 
 public class ActivitySelection {
 
+	static int INT_MAX = Integer.MAX_VALUE;
+	static int INT_MIN = Integer.MIN_VALUE;
+
 	static void recursiveSelection(int[] start, int[] end, int i, int j) {
 		if (i >= j) {
 			return ;
@@ -36,40 +39,43 @@ public class ActivitySelection {
 		System.out.print("A" + n + " ");
 	}
 
-	//???
 	static void dynamicSelector(int[] start, int[] end, int n) {
-		int[][] c = new int[n+2][n+2];
+		int[][] count = new int[n+2][n+2];
 		int[][] act = new int[n+2][n+2];
 
-        for (int i=0; i<=n+1; i++) {
-            for (int j=n+1; j>=i; j--) {
-                c[i][j] = 0;
-            }
-        }
-
-        int maxTemp = 0;
-        for (int j=1; j<=n+1; j++) {
-            for (int i=0; i<j; i++) {
-                for (int k=i+1; k<j; k++) {
-                    if (start[k] >= end[i] && end[k] <= start[j]) {
-                        if(c[i][k] + c[k][j] + 1 > maxTemp) {
-                            maxTemp = c[i][k] + c[k][j] + 1;
-                        }
-                    }
-                }
-                c[i][j] = maxTemp;
-                maxTemp = 0;
-            }
-        }
-		printActivity(c, act, 0, n+1);
+		for (int i=0; i<n+1; i++) {
+			count[i][i] = 0;
+			count[i][i+1] = 0;
+		}
+		count[n+1][n+1] = 0;
+		int j, k;
+		for (int Len=2; Len<=n+1; Len++) {
+			for (int i=0; i<=n-Len+1; i++) {
+				j = i + Len;
+				count[i][j] = 0;
+				k = j - 1;
+				while (end[i] < end[k]) {
+					if (end[i] <= start[k] && end[k] <= start[j]) {
+						if (count[i][k] + count[k][j] + 1 > count[i][j]) {
+							count[i][j] = count[i][k] + count[k][j] + 1;
+							act[i][j] = k;
+						}
+					}
+					k--;
+				}
+			}
+		}
+		System.out.println("A maximum size set of mutually compatible activities has size " + count[0][n+1]);
+		System.out.print("The set contains: ");
+		printActivity(count, act, 0, n+1);
 	}
 
 	static void printActivity(int[][] c, int[][] act, int i, int j) {
 		if (c[i][j] > 0) {
 			int k = act[i][j];
-			System.out.print("A" + k + " ");
 			printActivity(c, act, i, k);
 			printActivity(c, act, k, j);
+			System.out.print("A" + k + " ");
 		}
 	}
 
@@ -77,13 +83,15 @@ public class ActivitySelection {
 		//This program is to select best activities in order.
 		//In other words, it doesn't move back and forth, so FIFO.
 		//Also, this is not a correct method for getting best activity set within all activities.
-		int[] startTime = {-1,1,3,0,5,3,5,6,8,8,2,12};
-		int[] endTime = {-1,4,5,6,7,8,9,10,11,12,13,14};
+		int[] startTime = {0,1,3,0,5,3,5,6,8,8,2,12,INT_MAX};
+		int[] endTime = {0,4,5,6,7,9,9,10,11,12,14,16,INT_MIN};
+		//The last dummy time is not necessary for recursive and greedy;
+		//but it is necessary for dynamic, in case "j" goes out-of-bound;
 		recursiveSelection(startTime, endTime, 0, startTime.length);
 		System.out.println();
 		greedySelection(startTime, endTime);
 		System.out.println();
-		dynamicSelector(startTime, endTime, startTime.length);
+		dynamicSelector(startTime, endTime, startTime.length-2);
 	}
 
 }

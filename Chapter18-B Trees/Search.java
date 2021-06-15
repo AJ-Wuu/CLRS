a1797fe19f026d395b8f8f54361b3ec451650ad9
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-15 22:44:26 +0800

    Create Search.java

---
+++ Chapter18-B Trees/Search.java
@@ -0,0 +1,25 @@
+/**
+ * @author AJWuu
+ */
+
+package bTrees;
+
+public class Search {
+	
+	public static BTNode search(BTNode x, int k) {
+		int i = 0;
+		while (i <= x.n && k > x.keys[i]) {
+			i++;
+		}
+		if (i <= x.n && k == x.keys[i]) {
+			return x;
+		}
+		else if (x.isLeaf) { //no children
+			return null;
+		}
+		else {
+			return search(x.children[i], k);
+		}
+	}
+	
+}

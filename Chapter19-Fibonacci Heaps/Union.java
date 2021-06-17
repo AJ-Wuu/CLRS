eaa1dd2c2381dd571f9608630fb64babab1907fe
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-17 21:12:17 +0800

    Update Union.java

--- Chapter19-Fibonacci Heaps/Union.java
+++ Chapter19-Fibonacci Heaps/Union.java
@@ -4,47 +4,24 @@
 
 package fibonacciHeaps;
 
-public class TestHeap {
-
-	public static void main(String[] args) {
-		FibonacciHeap heap = new FibonacciHeap(0, 0, null, null);
-		Insert.insertKey(heap, 4);
-		Insert.insertKey(heap, 3);
-		Insert.insertKey(heap, 7);
-		Insert.insertKey(heap, 5);
-		Insert.insertKey(heap, 2);
-		Insert.insertKey(heap, 1);
-		Insert.insertKey(heap, 10);
-
-		System.out.println("**************Begin**************");
-		Display.display(heap.min);
-		System.out.println();
-		
-		System.out.println("\n**************Insert*************");
-		Insert.insertKey(heap, 6);
-		Insert.insertKey(heap, 9);
-		Insert.insertKey(heap, 8);
-		Display.display(heap.min);
-		System.out.println();
-		
-		System.out.println("\n***********Extract Min***********");
-		Min.extractMin(heap);
-		Display.display(heap.min);
-		System.out.println();
-		
-		System.out.println("\n**************Search*************");
-		System.out.println("Search for key = 2: " + Search.search(heap.min, 2));
-		System.out.println("Search for key = 9: " + Search.search(heap.min, 9));
-		System.out.println("Search for key = 12: " + Search.search(heap.min, 12));
-		
-		Display.display(heap.min);
-		System.out.println();
-		System.out.println("\n**************Delete*************");
-		Delete.delete(heap, 7);
-		Display.display(heap.min);
-		System.out.println();
-		
-		System.out.println("\n*************The End*************");
+public class Union {
+	
+	public static FibonacciHeap union(FibonacciHeap fh1, FibonacciHeap fh2) {
+		FibonacciHeap fh = new FibonacciHeap(0, 0, null, null);
+		fh.min = fh1.min;
+		if (fh1.min != null && fh2.min != null) {
+			FiboNode temp1 = fh1.min.left;
+			FiboNode temp2 = fh2.min.left;
+			fh1.min.left = temp2;
+			temp2.right = fh1.min;
+			fh2.min.left = temp1;
+			temp1.right = fh2.min;
+		}
+		if (fh1.min == null || (fh2.min != null && fh2.min.key < fh1.min.key)) {
+			fh.min = fh2.min;
+		}
+		fh.num = fh1.num + fh2.num;
+		return fh;
 	}
 
 }

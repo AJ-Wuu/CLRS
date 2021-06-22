6ad2e05aea879ca77d91191f70229dc65675781f
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-22 21:24:01 +0800

    Create DepthFirstSearch.java

---
+++ Chapter22-Elementary Graph Algorithms/DepthFirstSearch.java
@@ -0,0 +1,34 @@
+/**
+ * @author AJWuu
+ */
+
+package graph;
+
+import java.util.Iterator;
+
+public class DepthFirstSearch {
+
+	public static void DFSList(AdjacentList graph, int key, boolean[] visited) {
+		visited[key] = true;
+		System.out.print(key + " ");
+		
+		Iterator<Integer> i = graph.adj[key].iterator();
+		while (i.hasNext()) {
+			int next = i.next();
+			if (!visited[next]) {
+				DFSList(graph, next, visited);
+			}
+		}
+	}
+	
+	public static void DFSMatrix(AdjacentMatrix graph, int key, boolean[] visited) {
+		System.out.print(key + " ");
+		visited[key] = true;
+		for (int i=0; i<graph.getEdge(); i++) {
+			if (graph.adj[key][i] == 1 && !visited[i]) {
+				DFSMatrix(graph, i, visited);
+			}
+		}
+	}
+
+}

49b0ca99cdda306a008b7cbead5e96f91b336e44
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-22 21:24:25 +0800

    Create BreadthFirstSearch.java

---
+++ Chapter22-Elementary Graph Algorithms/BreadthFirstSearch.java
@@ -0,0 +1,55 @@
+/**
+ * @author AJWuu
+ */
+
+package graph;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class BreadthFirstSearch {
+
+	public static void BFSList(AdjacentList graph, int key) {
+		boolean visited[] = new boolean[graph.getVertex()];
+		LinkedList<Integer> bfs = new LinkedList<Integer>();
+		visited[key] = true;
+		bfs.add(key);
+		
+		while (bfs.size() != 0) {
+			key = bfs.poll(); //dequeue
+			System.out.print(key + " ");
+			
+			//iterator has some great helper functions!!!
+			Iterator<Integer> iter = graph.adj[key].iterator();
+			while (iter.hasNext()) {
+				int n = iter.next();
+				if (!visited[n]) { //not visited
+					visited[n] = true;
+					bfs.add(n);
+				}
+			}
+		}
+	}
+	
+	public static void BFSMatrix(AdjacentMatrix graph, int key) {
+		boolean[] visited = new boolean[graph.getEdge()];
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(key);
+		visited[key] = true;
+		
+		int temp;
+		while (!list.isEmpty()) {
+			temp = list.get(0);
+			System.out.print(temp + " ");
+			list.remove(list.get(0));
+			
+			for (int i=0; i<graph.getEdge(); i++) {
+				if (graph.adj[temp][i] == 1 && !visited[i]) {
+					list.add(i);
+					visited[i] = true;
+				}
+			}
+		}
+	}
+	
+}

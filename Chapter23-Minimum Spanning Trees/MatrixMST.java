e3e28ee4329a9c272fc2066db4828ff9ee67dc8c
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-06-26 20:42:32 +0800

    Create MatrixMST.java

---
+++ Chapter23-Minimum Spanning Trees/MatrixMST.java
@@ -0,0 +1,15 @@
+/**
+ * @author AJWuu
+ */
+
+package minimumSpanningTrees;
+
+public class MatrixMST {
+	int V;
+	double[][] edges;
+	
+	public MatrixMST(int V, double[][] edges) {
+		this.V = V;
+		this.edges = edges;
+	}
+}

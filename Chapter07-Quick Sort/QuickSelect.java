c5595fb7c75d86dfad2dde7719049070fd659e5f
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-26 21:10:24 +0800

    Update QuickSelect.java

--- Chapter07-Quick Sort/QuickSelect.java
+++ Chapter07-Quick Sort/QuickSelect.java
@@ -1,2 +1,3 @@
 //Quickselect (Hoare's Selection Algorithm)
 //It's typically used to solve the problems "find kth something": kth smallest, kth largest, kth most frequent, kth less frequent, etc.
+//It has O(N) average time complexity. Though its worth case time complexity is O(N^2), the probability of this worst-case is negligible.

2dd29e72c62eff650ab522d2a83aeebe900fb678
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-01 07:09:00 +0800

    Update EulerianPath_HierholzerAlgorithm.java

--- Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
+++ Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
@@ -13,3 +13,90 @@
 //      Trick here -> once we get stuck (that the current node has no unvisited outgoing edges), we BACKTRACK and add the current node to the solution
 //                    when backtracking, if the current node has any remaining unvisited edges, we follow any of them calling the DFS recursively to extend the Eulerian path
 //                    we can use the out[] array as the signal to see if unvisited node exists -> every time an edge is taken, reduce its outgoing edge count
+
+/**
+ * @author AJWuu
+ */
+
+package eulerianPath;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+class InOut {
+	int in;
+	int out;
+	
+	public InOut(int in, int out) {
+		this.in = in;
+		this.out = out;
+	}
+}
+
+public class EulerianPath {
+
+	/*
+	 * Note: 1. A graph can have multiple Eulerian Paths
+	 *       2. This code is for directed graph
+	 */
+	
+	public static ArrayList<Integer> DFS(HashMap<Integer, PriorityQueue<Integer>> graph, int curr, ArrayList<Integer> solution) {
+		while (!graph.get(curr).isEmpty()) {
+			int next = graph.get(curr).poll();
+			DFS(graph, next, solution);
+		}
+		solution.add(0, curr);
+		return solution;
+	}
+	
+	public static ArrayList<Integer> eulerianPath(int[][] nums) {
+		//Make graph map
+		HashMap<Integer, PriorityQueue<Integer>> graph = new HashMap<Integer, PriorityQueue<Integer>>();
+		HashMap<Integer, InOut> degrees = new HashMap<Integer, InOut>();
+		int sumIn = 0, sumOut = 0;
+		InOut temp;
+		for (int[] num : nums) {
+			graph.computeIfAbsent(num[0], k -> new PriorityQueue<Integer>());
+			graph.get(num[0]).add(num[1]);
+			
+			//Create / Update the InOut of each node
+			degrees.computeIfAbsent(num[0], k -> new InOut(0,0));
+			temp = degrees.get(num[0]);
+			temp.out++;
+			degrees.computeIfAbsent(num[1], k -> new InOut(0,0));
+			temp = degrees.get(num[1]);
+			temp.in++;
+			sumIn++;
+			sumOut++;
+		}
+		
+		//See if Eulerian Path exists
+		if (sumIn != sumOut) {
+			return (new ArrayList<Integer>());
+		}
+		int start = 0;
+		for (int key : degrees.keySet()) {
+			temp = degrees.get(key);
+			if (temp.in - temp.out > 1 || temp.out - temp.in > 1) {
+				return (new ArrayList<Integer>());
+			}
+			else if (temp.out - temp.in == 1) { //find the start node
+				start = key;
+			}
+		}
+		
+		//DFS
+		ArrayList<Integer> solution = new ArrayList<Integer>();
+		return DFS(graph, start, solution);
+	}
+	
+	public static void main(String[] args) {
+		int[][] nums = {{1,2},{1,3},{2,2},{2,4},{2,4},{3,1},{3,2},{3,5},{4,3},{4,6},{5,6},{6,3}};
+		ArrayList<Integer> result = eulerianPath(nums);
+		for (int i : result) {
+			System.out.print(i + " ");
+		}
+	}
+
+}

fd4786b09c9aeca0252109be51ef7fbd121a34e6
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-01 04:15:52 +0800

    Update EulerianPath_HierholzerAlgorithm.java

--- Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
+++ Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
@@ -1,7 +1,10 @@
-//Graph Theory, known as Eulerian Path / Circuit algorithm / Hierholzer's algorithm
+//A Graph Theory, known as Eulerian Path / Circuit algorithm / Hierholzer's algorithm
 //Step 1: Find an Eulerian path is determining, if there exists and Eulerian path
 //      1.1. Count the in/out degrees of each node by looping through all the edges -> make a int[N][3] with degree[n][0] = Node.index, degree[n][1] = in, degree[n][2] = out
 //      1.2. Verify that
 //           1.2.1. no node has too many in-/out-going edges (abs(in - out) > 1)
 //           1.2.2. sum(in) = sum(out)
 //Step 2: Find a valid starting node
+//      2.1. If we can find that Node A is the only node with exactly one extra out-going edge, then Node A is the start node (in[A] - out[A] = 1)
+//           If we can find that Node B is the only node with exactly one extra in-going edge, then Node B is the end node (out[B] = in[B] = 1)
+//      2.2. If all in and out degrees are equal, then the graph is an Euleran Circuit Case, so any node with non-zero degree could be the start node

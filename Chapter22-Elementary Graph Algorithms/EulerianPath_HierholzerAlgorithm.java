37c40cf4ce4a6f78eba4382987ce25507d855c0d
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-01 04:26:35 +0800

    Update EulerianPath_HierholzerAlgorithm.java

--- Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
+++ Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
@@ -1,4 +1,5 @@
 //A Graph Theory, known as Eulerian Path / Circuit algorithm / Hierholzer's algorithm
+//Time Complexity: O(E)
 //Step 1: Find an Eulerian path is determining, if there exists and Eulerian path
 //      1.1. Count the in/out degrees of each node by looping through all the edges -> make a int[N][3] with degree[n][0] = Node.index, degree[n][1] = in, degree[n][2] = out
 //      1.2. Verify that
@@ -11,3 +12,4 @@
 //Step 3: Modify the DFS to force it to traverse all the edges
 //      Trick here -> once we get stuck (that the current node has no unvisited outgoing edges), we BACKTRACK and add the current node to the solution
 //                    when backtracking, if the current node has any remaining unvisited edges, we follow any of them calling the DFS recursively to extend the Eulerian path
+//                    we can use the out[] array as the signal to see if unvisited node exists -> every time an edge is taken, reduce its outgoing edge count

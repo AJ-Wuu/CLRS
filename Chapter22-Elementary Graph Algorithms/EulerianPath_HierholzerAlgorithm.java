2c6633d28cb1c83b66c103971c85289075371e7b
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-01 05:43:10 +0800

    Update EulerianPath_HierholzerAlgorithm.java

--- Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
+++ Chapter22-Elementary Graph Algorithms/EulerianPath_HierholzerAlgorithm.java
@@ -7,7 +7,7 @@
 //           1.2.2. sum(in) = sum(out)
 //Step 2: Find a valid starting node
 //      2.1. If we can find that Node A is the only node with exactly one extra out-going edge, then Node A is the start node (in[A] - out[A] = 1)
-//           If we can find that Node B is the only node with exactly one extra in-going edge, then Node B is the end node (out[B] = in[B] = 1)
+//           If we can find that Node B is the only node with exactly one extra in-going edge, then Node B is the end node (out[B] - in[B] = 1)
 //      2.2. If all in and out degrees are equal, then the graph is an Euleran Circuit Case, so any node with non-zero degree could be the start node
 //Step 3: Modify the DFS to force it to traverse all the edges
 //      Trick here -> once we get stuck (that the current node has no unvisited outgoing edges), we BACKTRACK and add the current node to the solution
